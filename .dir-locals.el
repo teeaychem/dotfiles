2e2cd2e67b7e04fa2f548c5ca39e519829ef51d2
teeaychem <bsparkes@alumni.stanford.edu>
2026-06-09 23:02:13 +0100

    dir-locals : relax rg and fd

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+((nil
+  . ((consult-ripgrep-args
+      . ((consult--build-args (default-value 'consult-ripgrep-args))
+         "--hidden"
+         "--glob=!.git"))
+     (consult-fd-args
+      . ((consult--build-args (default-value 'consult-fd-args))
+         "--hidden"
+         "--exclude=.git")))))
